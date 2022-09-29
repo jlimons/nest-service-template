a52d38298e24097213c0809cc81c25429e4c66ba
jlimons <jalisa2808@gmail.com>
2022-09-29 13:24:40 -0500

    add docker-compose yml

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const host = process.env.HOST ?? '0.0.0.0';
+  const port = +(process.env.PORT ?? 8080);
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port, host);
 }
 bootstrap();
